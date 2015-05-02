60e884f6b51cc72b4259d665c36d9cf9a78127b3
GaoMatrix <gcquan08@gmail.com>
2015-05-02 21:18:04 +0800

    Add choose file dialog

--- src/com/gao/text/MainTextProcessing.java
+++ src/com/gao/text/MainTextProcessing.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JFileChooser;
+
 public class MainTextProcessing {
     private static final String[] mFeelingsCN = {
             "【微笑】", "【惊讶】", "【大笑】", "【伤心】", "【苦笑】"
@@ -29,23 +31,40 @@ public class MainTextProcessing {
     private static String mNormalFeeling = "";
 
     public static void main(String[] args) {
+        File file = null;
+        JFileChooser jfc = new JFileChooser();
+        jfc.setDialogTitle("选择一个目录");
+        jfc.setDialogType(JFileChooser.OPEN_DIALOG);
+        jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int res = jfc.showOpenDialog(null);
+        if (res == JFileChooser.APPROVE_OPTION) {
+            file = jfc.getSelectedFile();
+            System.out.println(file.getAbsolutePath());
+        }
 
+        if (file == null) {
+            return;
+        }
         FileUtils fileUtils = new FileUtils();
-        File file = new File("F:/小路/before.txt");
         try {
-            mBeforeList = fileUtils.readLines(file);
+            mBeforeList = fileUtils.readLines(file, "utf-8");
         } catch (IOException e) {
             e.printStackTrace();
         }
         for (String text : mBeforeList) {
             System.out.println(text);
         }
+        System.out.println("------------------before------------------");
 
         for (String text : mBeforeList) {
+            
+            System.out.println("------------------1------------------");
             if (text.contains("时间：")) {
                 mAfterList.add("#label day51:");
             }
+            System.out.println("------------------2------------------");
             if (text.contains("场景：")) {
+                System.out.println("------------------3------------------");
                 mAfterList.add("    #scene garden");
                 mAfterList.add("    #with fade");
                 mAfterList.add("    #play music \"music\\3.bgm.mp3\" fadein 1");
@@ -54,6 +73,7 @@ public class MainTextProcessing {
             }
 
             if (containFeeling(text)) {
+                System.out.println("------------------4------------------");
                 String actor = getActor(text);
                 String feeling = getFeeling(text);
                 if (isMainActor(actor)) {
@@ -75,12 +95,16 @@ public class MainTextProcessing {
 
         }
 
+        System.out.println("------------------5-ssss-----------------");
         for (String text : mAfterList) {
+            System.out.println("------------------511------------------");
             System.out.println(text);
         }
         try {
-            fileUtils.writeLines(new File("after.txt"), mAfterList);
+            System.out.println("------------------6------------------");
+            fileUtils.writeLines(new File("D:\\after.txt"), mAfterList);
         } catch (IOException e) {
+            System.out.println("------------------7------------------");
             e.printStackTrace();
         }
 
